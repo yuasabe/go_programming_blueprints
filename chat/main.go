aa552d745001be55e9676aeac27595994be229a7
Yu Asabe <yuasabe@g.ecc.u-tokyo.ac.jp>
2020-03-30 18:01:25 +0900

    Separate views from login using templates and use own handlers

--- chat/main.go
+++ chat/main.go
@@ -3,22 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					Let's Chat!
-				</body>
-			</html>
-		`))
-	})
+// temp1 represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	temp1    *template.Template
+}
 
+// ServerHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.temp1.Execute(w, nil)
+}
+
+func main() {
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServer:", err)
